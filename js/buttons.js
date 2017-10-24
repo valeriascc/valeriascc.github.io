1aa591024c444e1cfe3f5f75a31333bc892e3913
valeriascc <camarillo_valeria@hotmail.com>
2017-10-23 20:06:07 -0500

    new-changes

--- js/buttons.js
+++ js/buttons.js
@@ -1,85 +1,75 @@
-function lineaBtn() {
-   document.getElementById('med-capacidades').style.display = "none";
-   document.getElementById('lin-capacidades').style.display = "block";
-   document.getElementById('info-1').style.display = "none";
-   document.getElementById('info-2').style.display = "none";
-   document.getElementById('info-3').style.display = "none";
-   document.getElementById('slides').style.display = "none";
-   document.getElementById('slides2').style.display = "none";
-   document.getElementById('slides3').style.display = "none";
-   document.getElementById('slides4').style.display = "none";
-   document.getElementById('slides5').style.display = "none";
-
-}
-
-function medidaBtn(){
-	document.getElementById('med-capacidades').style.display = "block";
-	document.getElementById('lin-capacidades').style.display = "none";
-	document.getElementById('info-4').style.display = "none";
-   document.getElementById('info-5').style.display = "none";
-   document.getElementById('slides2').style.display = "none";
-   document.getElementById('slides3').style.display = "none";
-   document.getElementById('slides4').style.display = "none";
-   document.getElementById('slides5').style.display = "none";
-
+function linea() {
+	document.getElementById('lineaContainer').style.display = "block";
+	document.getElementById('slides').style.display = "none";
+	document.getElementById('med-description').style.display = "none";
+	document.getElementById('alta-description').style.display = "none";
+	document.getElementById('slides2').style.display = "none";
+	document.getElementById('metodo-imagen').style.display = "none";
+	document.getElementById('metodo-descripcion').style.display = "none";
+	document.getElementById('medida-container').style.display = "none";
+	document.getElementById('casos-seccion').style.display = "none";
 }
 
 function medianaBtn() {
-	document.getElementById('info-1').style.display = "block";
-	document.getElementById('info-2').style.display = "none";
-	document.getElementById('info-3').style.display = "none";
+	document.getElementById('lineaContainer').style.display = "block";
 	document.getElementById('slides').style.display = "block";
+	document.getElementById('alta-description').style.display = "none";
 	document.getElementById('slides2').style.display = "none";
+	document.getElementById('med-description').style.display = "block";
+	document.getElementById('maxima-description').style.display = "none";
 	document.getElementById('slides3').style.display = "none";
-	document.getElementById('slides4').style.display = "none";
-	document.getElementById('slides5').style.display = "none";
-
-}
-
-
-function maximaBtn() {
-	document.getElementById('info-2').style.display = "block";
-	document.getElementById('info-1').style.display = "none";
-	document.getElementById('info-3').style.display = "none";
-	document.getElementById('slides').style.display = "none";
-	document.getElementById('slides2').style.display = "block";
-	document.getElementById('slides3').style.display = "none";
-	document.getElementById('slides4').style.display = "none";
-	document.getElementById('slides5').style.display = "none";
-
 }
 
 function altaBtn() {
-	document.getElementById('info-3').style.display = "block";
-	document.getElementById('info-1').style.display = "none";
-	document.getElementById('info-2').style.display = "none";
+	document.getElementById('alta-description').style.display = "block";
+	document.getElementById('slides2').style.display = "block";
+	document.getElementById('lineaContainer').style.display = "block";
+	document.getElementById('med-description').style.display = "none";
 	document.getElementById('slides').style.display = "none";
-	document.getElementById('slides2').style.display = "none";
-	document.getElementById('slides3').style.display = "block";
-	document.getElementById('slides4').style.display = "none";
-	document.getElementById('slides5').style.display = "none";
-
+	document.getElementById('maxima-description').style.display = "none";
+	document.getElementById('slides3').style.display = "none";
 }
 
 function metodoBtn() {
-	document.getElementById('info-4').style.display = "block";
-	document.getElementById('info-5').style.display = "none";
-	document.getElementById('slides').style.display = "none";
-	document.getElementById('slides2').style.display = "none";
+	document.getElementById('metodo-imagen').style.display = "block";
+	document.getElementById('metodo-descripcion').style.display = "block";
+	document.getElementById('casos-seccion').style.display = "none";
+	document.getElementById('maxima-description').style.display = "none";
 	document.getElementById('slides3').style.display = "none";
-	document.getElementById('slides4').style.display = "block";
-	document.getElementById('slides5').style.display = "none";
+	document.getElementById('lineaContainer').style.display = "none";
+	document.getElementById('slides').style.display = "none";
+}
 
+function medida() {
+	document.getElementById('medida-container').style.display = "block";
+	document.getElementById('lineaContainer').style.display = "none";
+	document.getElementById('slides').style.display = "none";
+	document.getElementById('metodo-imagen').style.display = "none";
+	document.getElementById('metodo-descripcion').style.display = "none";
+	document.getElementById('maxima-description').style.display = "none";
+	document.getElementById('slides3').style.display = "none";
+	document.getElementById('med-description').style.display = "none";
+	document.getElementById('alta-description').style.display = "none";
+	document.getElementById('slides2').style.display = "none";
 }
 
 function casosBtn() {
-	document.getElementById('info-5').style.display = "block";
-	document.getElementById('info-4').style.display = "none";
+	document.getElementById('casos-seccion').style.display = "block";
+	document.getElementById('medida-container').style.display = "block";
+	document.getElementById('lineaContainer').style.display = "none";
 	document.getElementById('slides').style.display = "none";
-	document.getElementById('slides2').style.display = "none";
+	document.getElementById('metodo-imagen').style.display = "none";
+	document.getElementById('metodo-descripcion').style.display = "none";
+	document.getElementById('maxima-description').style.display = "none";
 	document.getElementById('slides3').style.display = "none";
-	document.getElementById('slides4').style.display = "none";
-	document.getElementById('slides5').style.display = "block";
+}
 
-
+function maximaBtn() {
+	document.getElementById('maxima-description').style.display = "block";
+	document.getElementById('slides3').style.display = "block";
+	document.getElementById('alta-description').style.display = "none";
+	document.getElementById('slides2').style.display = "none";
+	document.getElementById('lineaContainer').style.display = "block";
+	document.getElementById('med-description').style.display = "none";
+	document.getElementById('slides').style.display = "none";
 }
